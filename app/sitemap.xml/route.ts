2dd3afbbcc235b83afe5f8dd524d1021a50e0266
ALA <ALA>
2026-05-25 07:10:23 +0200

    Force sitemap XML route headers

--- app/sitemap.xml/route.ts
+++ app/sitemap.xml/route.ts
@@ -1,7 +1,7 @@
 import { buildSitemapXml } from "@/lib/sitemapXml"
 
-export const dynamic = "force-static"
-export const revalidate = 3600
+export const dynamic = "force-dynamic"
+export const revalidate = 0
 
 export function GET() {
   return new Response(buildSitemapXml(), {
